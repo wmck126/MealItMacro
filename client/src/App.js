8cecef09eccf66fa6f4a9a5c3fc5708395708356
Ward McKinley <wmck126@gmail.com>
2022-09-14 20:00:38 -0400

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch("/hello")
-    .then((r) => r.json())
-    .then((data) => setCount(data.count))
-  }, [])
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Page count: {count}</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/testing">
+            <h1>Test Route</h1>
+          </Route>
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
