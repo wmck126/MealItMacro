93b69fb87ae9ff802278a9611637ed032255fb53
Ward McKinley <wmck126@gmail.com>
2022-09-21 15:39:12 -0400

    Fixed client side routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <HashRouter>
+        <BrowserRouter>
         <App />
-        </HashRouter>
+        </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  # get '*path',
-  #     to: 'fallback#index',
-  #     constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
